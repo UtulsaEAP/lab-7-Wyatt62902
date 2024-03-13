43671001d9b811d1200cf10da9be556931afd02c
Wyatt62902 <156848807+Wyatt62902@users.noreply.github.com>
2024-03-13 12:16:20 -0500

    problem 1

--- Problem 1/exceptionHandling.py
+++ Problem 1/exceptionHandling.py
@@ -1,12 +1,20 @@
+'''
+Name: Wyatt Fulton
+Lab Time: Thur 2:00 PM
+'''
+
 def exceptionHandling():
     # Split input into 2 parts: name and age
     parts = input().split()
     name = parts[0]
     while name != '-1':
-        # FIXME: The following line will throw ValueError exception.
-        #        Insert try/except blocks to catch the exception.
-        age = int(parts[1]) + 1
-        print(f'{name} {age}')
+        try:
+            age = int(parts[1]) + 1
+            print(f'{name} {age}')
+        except: 
+            age = 0
+            print(f'{name} {age}')
+
         
         # Get next line
         parts = input().split()
