93a2a695e1745c8be24483eb42a1306e8f699750
Wyatt62902 <156848807+Wyatt62902@users.noreply.github.com>
2024-03-13 14:43:27 -0500

    problem 2

--- Problem 2/wordInRange.py
+++ Problem 2/wordInRange.py
@@ -1,5 +1,24 @@
+'''
+Name: Wyatt Fulton
+Lab Time: Thur 2:00 PM
+'''
+
 def wordInRange():
-    #Type your code here
+    inp_file = input()
+    start_line = input()
+    stop_bound = input()
+    with open(inp_file, 'r') as current_line:
+        for line in current_line:
+            line = line.strip('\n')
+            if line >= start_line:
+                if line <= stop_bound:
+                    print(line + ' - in range')
+                else:
+                    print(line+' - not in range')
+            else:
+                print(line+' - not in range')
+
+
     return
 if __name__ == '__main__':
     wordInRange()
