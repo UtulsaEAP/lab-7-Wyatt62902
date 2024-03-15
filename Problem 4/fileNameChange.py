2723139569c9e99ed5306a32b78be32f8ecd71fa
Wyatt62902 <156848807+Wyatt62902@users.noreply.github.com>
2024-03-15 14:47:07 -0500

    problem 4

--- Problem 4/fileNameChange.py
+++ Problem 4/fileNameChange.py
@@ -1,5 +1,26 @@
+'''
+Name: Wyatt Fulton
+Lab Time: Thur 2:00 PM
+'''
+
 def fileNameChange():
-    #type your code here
+    
+    file_name = input()
+    updated_file = file_name
+    accumulator = 0
+    newfile_list = []
+
+
+    with open(file_name, '+r') as file_name:
+        for line in file_name:
+            line = str(line).split("_")
+            update_line = line[0]+'_info.txt\n'
+            newfile_list.append(update_line)
+
+    with open(updated_file, 'w') as updated_file:
+        for i in newfile_list:
+            updated_file.write(i)
+            
 
     return
 
