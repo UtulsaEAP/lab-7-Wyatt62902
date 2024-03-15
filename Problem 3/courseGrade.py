4077c7a834913cc1b911ff2b64645d7c4f889835
Wyatt62902 <156848807+Wyatt62902@users.noreply.github.com>
2024-03-15 12:14:14 -0500

    problem 3

--- Problem 3/courseGrade.py
+++ Problem 3/courseGrade.py
@@ -1,14 +1,21 @@
 def courseGrade():
-    
-    # TODO: Declare any necessary variables here. 
+    '''
+    Name: Wyatt Fulton
+    Date: Thur 2:00 PM
+    '''
       
     midt1_sum = 0
     midt2_sum = 0
-    fin_sum = 0  
+    fin_sum = 0
+    midt1_avg = 0
+    midt2_avg = 0
+    fin_avg = 0  
     line_avg = 0
+    num_lines = 0
     line_grade = ''
     file_name = input()
     write_file = ''
+
 
     if file_name == './Problem 3/StudentInfo.tsv':
         write_file = './Problem 3/report.txt'
@@ -19,14 +26,14 @@ def courseGrade():
 
 
     with open(file_name, 'r') as read_file:
-        with open(write_file, 'x'):
+        with open(write_file, 'w') as write_file:
             for line in read_file:
                 line = line.strip('\n')
-                line_list = line.split("\t")
+                line_list = line.split()
                 midt1_sum += int(line_list[2])
                 midt2_sum += int(line_list[3])
                 fin_sum += int(line_list[4])
-                line_avg = (line_list[2] + line_list[3] + line_list[4]) / 3
+                line_avg = (int(line_list[2]) + int(line_list[3]) + int(line_list[4])) / 3
                 if line_avg >= 90:
                     line_grade = 'A'
                 elif (line_avg < 90) and (line_avg >= 80):
@@ -38,14 +45,19 @@ def courseGrade():
                 else:
                     line_grade = 'F'
                 write_file.write(str(line_list[0])+'\t'+str(line_list[1])+'\t'+str(line_list[2])+'\t'+str(line_list[3])+'\t'+str(line_list[4])+'\t'+line_grade+'\n')
-    print(write_file.read())
+                num_lines += 1
+            midt1_avg = float(midt1_sum / num_lines)
+            midt2_avg = float(midt2_sum / num_lines)
+            fin_avg = float(fin_sum / num_lines)
+            write_file.write(f"Averages: midterm1{midt1_avg:.2f}, midterm2{midt2_avg:.2f}, final{fin_avg:.2f}\n")
                 
 
-   
-    # TODO: Compute student grades and exam averages, then output results to a text file here. 
     return
 
 if __name__ == "__main__":
     courseGrade()
+
+
+
     
     
