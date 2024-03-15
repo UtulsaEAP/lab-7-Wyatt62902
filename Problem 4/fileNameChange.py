aa17f3e3f2438a5b79af342d73dfba699489ab26
Wyatt62902 <156848807+Wyatt62902@users.noreply.github.com>
2024-03-15 15:09:47 -0500

    problem 4

--- Problem 4/fileNameChange.py
+++ Problem 4/fileNameChange.py
@@ -14,12 +14,13 @@ def fileNameChange():
     with open(file_name, '+r') as file_name:
         for line in file_name:
             line = str(line).split("_")
-            update_line = line[0]+'_info.txt\n'
+            update_line =line[0]+'_info.txt'+'\n'
             newfile_list.append(update_line)
 
     with open(updated_file, 'w') as updated_file:
         for i in newfile_list:
             updated_file.write(i)
+        updated_file.write('\b\b')
             
 
     return
